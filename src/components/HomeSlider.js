5459d5e2019490c4f9b42fc0426caca4caf30e2d
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2023-01-02 23:10:10 +0600

    Home Slider Part Done

---
+++ src/components/HomeSlider.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Slider1 from "../img/slider (1).jpg";
+import Slider2 from "../img/slider (2).jpg";
+import Slider3 from "../img/slider (3).jpg";
+
+const HomeSlider = () => {
+  return (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img alt="" src={Slider1} className="w-full" />
+        <div className="absolute h-full w-full bg-black opacity-70"></div>
+        <div className="absolute flex flex-col justify-center items-center h-full w-full">
+          <h1 className="text-[#71B002] text-5xl">Home Grow</h1>
+          <p className="text-white text-xl my-4">
+            Home Grow is an initiative in agriculture sector with a new vision
+            was incorporated
+          </p>
+          <button className="btn btn-outline btn-success">
+            Learn More
+          </button>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img alt="" src={Slider2} className="w-full" />
+        <div className="absolute h-full w-full bg-black opacity-70"></div>
+        <div className="absolute flex flex-col justify-center items-center h-full w-full">
+          <h1 className="text-[#71B002] text-5xl">Breeding</h1>
+          <p className="text-white text-xl my-4">
+            Breeding for high yielding varieties to adapt with climatic change.
+          </p>
+          <button className="btn btn-outline btn-success">
+            Learn More
+          </button>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img alt="" src={Slider3} className="w-full" />
+        <div className="absolute h-full w-full bg-black opacity-70"></div>
+        <div className="absolute flex flex-col justify-center items-center h-full w-full">
+          <h1 className="text-[#71B002] text-5xl">Export</h1>
+          <p className="text-white text-xl my-4">We export seed to Hongkong, Pakistan, Malaysia, S. Korea, China etc.</p>
+          <button className="btn btn-outline btn-success">
+            Learn More
+          </button>
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSlider;
