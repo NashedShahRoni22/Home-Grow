8c4ace38be889ea5fdd4ac4a35a16c626aed1aba
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2023-09-08 15:55:51 +0600

    updated product added

--- src/pages/SeedsProducts.js
+++ src/pages/SeedsProducts.js
@@ -16,9 +16,11 @@ const SeedsProducts = () => {
       <span className="text-[#71B002]">{seedsProduct?.length} Seeds</span> Products
       </h2>
       <div className="my-10 grid gap-6 justify-center md:grid-cols-2 lg:grid-cols-4">
-        {seedsProduct?.map((sp,i) => (
+                                                                                                                              
+                                                                                          
+        {seedsProduct?.map((sp,i) => (                                   
           <SeedsProductCard sp={sp} key={i}></SeedsProductCard>
-        ))}
+        ))}                                                                                                    
       </div>
     </div>
   );
