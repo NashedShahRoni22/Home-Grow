a6696a36e3e7c5c57a348ee479de026d8f1add85
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2024-03-01 02:12:05 +0600

    Banner Updated

--- src/components/HomeSlider.js
+++ src/components/HomeSlider.js
@@ -4,70 +4,57 @@ import Slider2 from "../img/slider (2).jpg";
 import Slider3 from "../img/slider (3).jpg";
 
 const HomeSlider = () => {
+  const slidersData = [
+    {
+      title: "Home Grow",
+      desc: "Welcome to Home Grow, where we cultivate organic seeds and promote rooftop gardening. Our vision is to empower individuals with sustainable agriculture solutions for a greener future.",
+      image: Slider1,
+    },
+    {
+      title: "Breeding",
+      desc: "Expanding our reach globally, we export premium organic seeds to Hong Kong, Pakistan, Malaysia, South Korea, and China. Join us in spreading greenery worldwide.",
+      image: Slider2,
+    },
+    {
+      title: "Export",
+      desc: "At Home Grow, we specialize in breeding high-yielding seed varieties resilient to climate change. Our commitment to innovation drives us to create superior seeds for sustainable farming practices.",
+      image: Slider3,
+    },
+  ];
   return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img alt="" src={Slider1} className="w-full" />
-        <div className="absolute h-full w-full bg-black opacity-70"></div>
-        <div className="absolute flex flex-col justify-center items-center h-full w-full">
-          <h1 className="text-[#71B002] text-5xl lg:text-7xl font-bold">
-            Home Grow
-          </h1>
-          <p className="text-white text-center md:text-3xl lg:text-5xl my-4 mx-5">
-            Home Grow is an initiative in agriculture sector with a new vision
-            was incorporated
-          </p>
-        </div>
-        <div className="absolute flex gap-4 bottom-5 right-5">
-          <a href="#slide3" className="btn btn-circle btn-outline btn-success">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle btn-outline btn-success">
-            ❯
-          </a>
-        </div>
+    <section>
+
+      <div className="carousel w-full lg:h-[80vh]">
+        {slidersData.map((s,i) => (
+          <div id={`slide${i}`} className="carousel-item relative w-full">
+            <img alt="" src={s.image} className="w-full" />
+            <div className="absolute h-full w-full bg-black opacity-70"></div>
+            <div className="absolute flex flex-col gap-5 lg:gap-10 justify-center h-full lg:w-1/2 mx-5 lg:mx-10">
+              <h1 className="text-[#71B002] text-xl md:text-3xl lg:text-5xl font-bold">
+                {s.title}
+              </h1>
+              <p className="text-white text-sm md:text-xl lg:text-5xl text-justify">
+                {s.desc}
+              </p>
+            </div>
+            <div className="absolute flex gap-4 bottom-2  md:bottom-5 right-2 md:right-5">
+              <a
+                href={`#slide${i-1}`}
+                className="btn btn-outline btn-success btn-circle btn-sm md:btn-md lg:btn-lg"
+              >
+                ❮
+              </a>
+              <a
+                href={`#slide${i+1}`}
+                className="btn btn-outline btn-success btn-circle btn-sm md:btn-md lg:btn-lg"
+              >
+                ❯
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img alt="" src={Slider2} className="w-full" />
-        <div className="absolute h-full w-full bg-black opacity-70"></div>
-        <div className="absolute flex flex-col justify-center items-center h-full w-full">
-          <h1 className="text-[#71B002] text-5xl lg:text-7xl font-bold">
-            Breeding
-          </h1>
-          <p className="text-white text-center md:text-3xl lg:text-5xl my-4 mx-5">
-            Breeding for high yielding varieties to adapt with climatic change.
-          </p>
-        </div>
-        <div className="absolute flex gap-4 bottom-5 right-5">
-          <a href="#slide1" className="btn btn-circle btn-outline btn-success">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle btn-outline btn-success">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img alt="" src={Slider3} className="w-full" />
-        <div className="absolute h-full w-full bg-black opacity-70"></div>
-        <div className="absolute flex flex-col justify-center items-center h-full w-full">
-          <h1 className="text-[#71B002] text-5xl lg:text-7xl font-bold">
-            Export
-          </h1>
-          <p className="text-white text-center md:text-3xl lg:text-5xl my-4 mx-5">
-            We export seed to Hongkong, Pakistan, Malaysia, S. Korea, China etc.
-          </p>
-        </div>
-        <div className="absolute flex gap-4 bottom-5 right-5">
-          <a href="#slide2" className="btn btn-outline btn-success btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-outline btn-success btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-    </div>
+    </section>
   );
 };
 
