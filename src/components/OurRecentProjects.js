e34e7fd2ee0b032995b635ad3e37664b71d544c1
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2024-10-07 12:36:43 +0600

    Updated Content

--- src/components/OurRecentProjects.js
+++ src/components/OurRecentProjects.js
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import usaImg from "../../src/img/usa.jpg";
+
 
 const OurRecentProjects = () => {
   const { data: projectsImages } = useQuery({
@@ -14,34 +14,18 @@ const OurRecentProjects = () => {
         <h1 className="text-4xl font font-semibold mb-10 md:mb-20 text-white text-center">
           Our <span className="text-[#71B002]">Recent Projects</span>
         </h1>
-        <div className="grid lg:grid-cols-2 gap-5">
-          <div className="relative">
-            <img src={usaImg} className="w-full rounded-t-2xl" alt="our_rp" />
-            {/* <div className="h-full w-full absolute top-0 rounded-2xl flex items-end">
-              <p className="text-white text-center mt-5 bg-black py-2.5 rounded-b-2xl">
-                Displaying home grow products consulate office of Bangladesh at
-                Los Angeles where consul General, commercial counselor and
-                representative were present there.
-              </p>
-            </div> */}
-            <p className="text-white text-center bg-black py-2.5 rounded-b-2xl">
-                Displaying home grow products consulate office of Bangladesh at
-                Los Angeles where consul General, commercial counselor and
-                representative were present there.
-              </p>
-          </div>
-          <div className="grid gap-5 md:grid-cols-2">
-            {projectsImages?.map((pi) => (
-              <img
-                src={pi.img}
-                alt="projectsImages"
-                className="rounded-2xl h-full"
-                // data-aos="flip-left"
-                // data-aos-easing="ease-out-cubic"
-                // data-aos-duration="2000"
-              />
-            ))}
-          </div>
+
+        <div className="grid gap-5 md:grid-cols-2">
+          {projectsImages?.map((pi) => (
+            <img
+              src={pi.img}
+              alt="projectsImages"
+              className="rounded-2xl h-full"
+              data-aos="flip-left"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="2000"
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/pages/JuteProducts.js
+++ src/pages/JuteProducts.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import usaImg from "../../src/img/usa.jpg";
 
 const JuteProducts = () => {
   const [jutesProduct, setJutesProduct] = useState([]);
@@ -9,20 +10,42 @@ const JuteProducts = () => {
   }, []);
   return (
     <div className="my-10 p-10 container mx-auto">
-      <h2 className="text-3xl font-semibold">
+      <div className="relative">
+        <p className="text-gray-700 text-lg md:text-xl mb-4 text-center font-semibold">
+          Displaying home grow products at the Consulate Office of Bangladesh in
+          Los Angeles, where the Consul General, Commercial Counselor, and
+          representatives were present.
+        </p>
+        <img
+          src={usaImg}
+          className="w-full max-w-[600px] h-auto mx-auto rounded-lg"
+          alt="our_rp"
+        />
+      </div>
+
+      <h2 className="text-3xl font-semibold mt-5">
         <span className="text-[#71B002]">{jutesProduct?.length} Jutes</span>{" "}
         Products
       </h2>
       <div className="my-10 grid gap-6 justify-center md:grid-cols-2 lg:grid-cols-4">
         {jutesProduct?.map((jp, i) => (
-          <div key={i} className="card glass" data-aos="flip-right" data-aos-duration="2000">
-          <figure>
-            <img src={jp.img} alt="SeedsProductCard" className="h-48 w-full"/>
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title text-[#71B002]">{jp.name}</h2>
+          <div
+            key={i}
+            className="card glass"
+            data-aos="flip-right"
+            data-aos-duration="2000"
+          >
+            <figure>
+              <img
+                src={jp.img}
+                alt="SeedsProductCard"
+                className="h-48 w-full"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-[#71B002]">{jp.name}</h2>
+            </div>
           </div>
-        </div>
         ))}
       </div>
     </div>
